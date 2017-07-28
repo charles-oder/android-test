2dd113aca6285aaeb590be971ba645b37b357e37
Charles Oder <charles.oder@dtn.com>
2017-07-27 21:56:31 -0500

    added tests

---
+++ app/src/main/java/us/oder/charles/androidexample/TextGiver.java
@@ -0,0 +1,12 @@
+package us.oder.charles.androidexample;
+
+/**
+ * Created by charlesoder on 7/27/17.
+ */
+
+public class TextGiver {
+
+    public String getTitle() {
+        return "Muahahahahaha";
+    }
+}
