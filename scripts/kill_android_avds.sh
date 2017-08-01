5d1c12a352423fba7e79d24cd13d129eb5ef33f8
Charles Oder <charles.oder@dtn.com>
2017-08-01 12:04:16 -0500

    updated kill script

--- scripts/kill_android_avds.sh
+++ scripts/kill_android_avds.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]
 fi
 
 # Kill all avds
-DEVICE_LIST=$(adb devices | sed "1 d" | tail -r | sed "1 d")
+DEVICE_LIST=$($1/platform-tools/adb devices | sed "1 d" | tail -r | sed "1 d")
 for DEVICE_NAME in $DEVICE_LIST
 do
 	$1/platform-tools/adb -s $DEVICE_NAME emu kill &
